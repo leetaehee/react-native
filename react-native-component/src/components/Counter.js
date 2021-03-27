9687f18492b031071b02040010959bf26cfe76c7
leetaehee <lastride25@naver.com>
2021-03-27 22:39:21 +0900

    여러 개의 useState

--- react-native-component/src/components/Counter.js
+++ react-native-component/src/components/Counter.js
@@ -4,11 +4,25 @@ import MyButton from './MyButtons';
 
 const Counter = () => {
     const [count, setCount] = useState(0);
+    const [double, setDouble] = useState(0);
     return (
         <View style={{ alignItems: 'center' }}>
-            <Text style={{ fontSize: 39, margin: 10 }}>{count}</Text>
-            <MyButton title="+1" onPress={() => setCount(count+1)} />
-            <MyButton title="-1" onPress={() => setCount(count-1)} />
+            <Text style={{ fontSize: 30, margin: 10 }}>count: {count}</Text>
+            <Text style={{ fontSize: 30, margin: 10 }}>double: {double}</Text>
+            <MyButton
+                title="+"
+                onPress={() => {
+                    setCount(count + 1);
+                    setDouble(double + 2);
+                }}
+            />
+            <MyButton 
+                title="-"
+                onPress={() => {
+                    setCount(count - 1);
+                    setDouble(double - 2);
+                }}
+            />
         </View>
     );
 };
