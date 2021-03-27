bc5f0d18b1942505b9ec04218a8b0be23e75e6ab
leetaehee <lastride25@naver.com>
2021-03-27 22:09:07 +0900

    useState 사용하기

--- react-native-component/src/App.js
+++ react-native-component/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import MyButton from './components/MyButtons';
+import Counter from './components/Counter';
 
 const App = () => {
     return (
@@ -12,12 +12,7 @@ const App = () => {
                 justifyContent: 'center',
             }}
         >
-            <Text style={{ fontSize: 30, marginBottom: 10 }}>Props</Text>
-            <MyButton title="Button" onPress={() => alert('props')} />
-            <MyButton title="Button" onPress={() => alert('children')}>
-                Children Props
-            </MyButton>
-            <MyButton onPress={() => alert('default')} />
+            <Counter />
         </View>
     );
 };
