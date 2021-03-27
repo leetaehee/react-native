62cdd95ed63dde976153e43b81275a5e954eb4c1
leetaehee <lastride25@naver.com>
2021-03-27 23:08:20 +0900

    onchange

--- react-native-component/src/App.js
+++ react-native-component/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import Counter from './components/Counter';
-import EventButton from './components/EventButton';
+import EventInput from './components/EventInput';
+import EventButton from './components/EventInput';
 
 const App = () => {
     return (
@@ -13,7 +14,7 @@ const App = () => {
                 justifyContent: 'center',
             }}
         >
-            <EventButton />
+            <EventInput />
         </View>
     );
 };
