ddf050d3318397e1cb54432503187f4b3ee87561
leetaehee <lastride25@naver.com>
2021-03-28 17:40:24 +0900

    리액트네이티브 스타일링 flex

--- react-native-style/src/App.js
+++ react-native-style/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { viewStyles, textStyles } from './styles';
+import { Header, Contents, Footer } from './components/Layout'; 
 
 const App = () => {
     return (
         <View style={viewStyles.container}>
-            <Text style={[textStyles.text, { color: 'green' }]}>Inline Styling - Text</Text>
-            <Text style={[textStyles.text, textStyles.error]}>Inline Styling - Error</Text>
+            <Header />
+            <Contents />
+            <Footer />
         </View>
     );
 };
