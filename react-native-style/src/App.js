94728931b8943b147ab6825cf2dea2353d9e2ac7
leetaehee <lastride25@naver.com>
2021-03-28 16:38:20 +0900

    인라인 스타일링

--- react-native-style/src/App.js
+++ react-native-style/src/App.js
@@ -3,8 +3,34 @@ import { View, Text } from 'react-native';
 
 const App = () => {
     return (
-        <View>
-            <Text>React Native Style</Text>
+        <View
+            style={{
+                flex: 1,
+                backgroundColor: '#fff',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            <Text 
+                style={{
+                    padding: 10,
+                    fontSize: 26,
+                    fontWeight: '600',
+                    color: 'black'
+                }}
+            >
+                Inline Styling - Text
+            </Text>
+            <Text
+                style={{
+                    padding: 10,
+                    fontSize: 26,
+                    fontWeight: '400',
+                    color: 'red',
+                }}
+            >
+                Inline Styling - Error
+            </Text>
         </View>
     );
 };
