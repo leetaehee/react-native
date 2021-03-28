e18cf75c1d626446d6af2e0517b6c31cc7c73603
leetaehee <lastride25@naver.com>
2021-03-28 18:35:49 +0900

    스타일드 컴포넌트  / prop

--- react-native-style/src/App.js
+++ react-native-style/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { viewStyles, textStyles } from './styles';
-import ShadowBox from './components/ShadowBox';
+import styled from 'styled-components/native';
+import Button from './components/Button';
+
+const Container = styled.View`
+    flex: 1;
+    background-color: #ffffff;
+    align-items: center;
+    justify-content: center;
+`;
 
 const App = () => {
     return (
-        <View style={viewStyles.container}>
-            <ShadowBox />
-        </View>
+        <Container>
+            <Button title="Hanbit" />
+            <Button title="React Native" />
+        </Container>
     );
 };
 
