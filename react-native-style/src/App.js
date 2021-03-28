86199095c77ea623681540585311edcf472804bd
leetaehee <lastride25@naver.com>
2021-03-28 20:08:02 +0900

    theme

--- react-native-style/src/App.js
+++ react-native-style/src/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import styled from 'styled-components/native';
+import React, { useState } from 'react';
+import { Switch } from 'react-native';
+import styled, { ThemeProvider } from 'styled-components/native';
 import Button from './components/Button';
 import Input from './components/Input';
+import { lightTheme, darkTheme } from './theme';
 
 const Container = styled.View`
     flex: 1;
-    background-color: #ffffff;
+    background-color: ${props => props.theme.background};
     align-items: center;
     justify-content: center;
 `;
 
 const App = () => {
+    const [isDark, setIsDark] = useState(false);
+    const _toggleSwitch = () => setIsDark(!isDark);
     return (
-        <Container>
-            <Button title="Hanbit" />
-            <Button title="React Native" />
-            <Input borderColor="#3498db" />
-            <Input borderColor="#9b59b6" />
-        </Container>
+        <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+            <Container>
+                <Switch value={isDark} onValueChange={_toggleSwitch} />
+                <Button title="Hanbit" />
+                <Button title="React Native" />
+                <Input borderColor="#3498db" />
+                <Input borderColor="#9b59b6" />
+            </Container>
+        </ThemeProvider>
     );
 };
 
