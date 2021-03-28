fcaeaafc7716f328909a2867884dceb2ed0e04db
leetaehee <lastride25@naver.com>
2021-03-28 16:50:58 +0900

    여러개의 클라스 스타일링 적용

--- react-native-style/src/App.js
+++ react-native-style/src/App.js
@@ -4,8 +4,8 @@ import { StyleSheet, View, Text } from 'react-native';
 const App = () => {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Inline Styling - Text</Text>
-            <Text style={styles.error}>Inline Styling - Error</Text>
+            <Text style={[styles.text, { color: 'green' }]}>Inline Styling - Text</Text>
+            <Text style={[styles.text, styles.error]}>Inline Styling - Error</Text>
         </View>
     );
 };
@@ -24,8 +24,6 @@ const styles = StyleSheet.create({
         color: 'black',
     },
     error: {
-        padding: 10,
-        fontSize: 26,
         fontWeight: '400',
         color: 'red',
     },
