18923cc9a2d0e1cbfd3013711001e94aea2ea61e
leetaehee <lastride25@naver.com>
2021-03-28 16:56:50 +0900

    외부 클래스 스타일링

--- react-native-style/src/App.js
+++ react-native-style/src/App.js
@@ -1,32 +1,14 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
+import { viewStyles, textStyles } from './styles';
 
 const App = () => {
     return (
-        <View style={styles.container}>
-            <Text style={[styles.text, { color: 'green' }]}>Inline Styling - Text</Text>
-            <Text style={[styles.text, styles.error]}>Inline Styling - Error</Text>
+        <View style={viewStyles.container}>
+            <Text style={[textStyles.text, { color: 'green' }]}>Inline Styling - Text</Text>
+            <Text style={[textStyles.text, textStyles.error]}>Inline Styling - Error</Text>
         </View>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {
-        padding: 10,
-        fontSize: 26,
-        fontWeight: '600',
-        color: 'black',
-    },
-    error: {
-        fontWeight: '400',
-        color: 'red',
-    },
-});
 
 export default App;
