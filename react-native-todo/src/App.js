39afb758f990875566b3a12f7fb1821fa9b43b6c
leetaehee <lastride25@naver.com>
2021-03-29 23:17:14 +0900

    할 일 애플리케이션 프론트엔드 작업중

---
+++ react-native-todo/src/App.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { StatusBar, Dimensions } from 'react-native';
+import styled, { ThemeProvider } from 'styled-components/native';
+import { theme } from './theme';
+import Input from './components/Input';
+import { images } from './images';
+import IconButton from './components/IconButton';
+import Task from './components/Task';
+
+const Container = styled.SafeAreaView`
+    flex: 1;
+    background-color: ${({ theme }) => theme.background };
+    align-items: center;
+    justify-content: flex-start;
+`;
+
+const Title = styled.Text`
+    font-size: 40px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.main};
+    align-self: flex-start;
+    margin: 0px 20px;
+`;
+
+const List = styled.ScrollView`
+    flex: 1;
+    width: ${({ width }) => width - 40}px;
+`;
+
+export default function App() {
+    const width = Dimensions.get('window').width;
+    const [newTask, setNewTask] = useState('');
+
+    const _addTask = () => {
+        alert(`Add: ${newTask}`);
+        setNewTask('');
+    };
+
+    const _handleTextChange = text => {
+        setNewTask(text);
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container>
+                <StatusBar
+                    barStyle="light-content"
+                    backgroundColor={theme.background}
+                />
+                <Title>TODO List</Title>
+                <Input 
+                    placeholder="+ Add a Task" 
+                    value={newTask}
+                    onChangeText={_handleTextChange}
+                    onSubmitEditing={_addTask}
+                />
+                <List width={width}>
+                    <Task text="Hanbit" />
+                    <Task text="React Native" />
+                    <Task text="React Native Sample" />
+                    <Task text="Edit TODO Item" />
+                </List>
+            </Container>
+        </ThemeProvider>
+    );
+}
