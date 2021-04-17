cade8f194e90134268fd50fd322cbaada3516a71
leetaehee <lastride25@naver.com>
2021-04-17 20:36:06 +0900

    탭 네비게이션 적용

--- react-native-navigation/src/App.js
+++ react-native-navigation/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigation from './navigations/Stack';
+import TabNavigation from './navigations/Tab';
 
 const App = () => {
+    return (
+        <NavigationContainer>
+            <TabNavigation />
+        </NavigationContainer>
+    );
+    /*
     return (
         <NavigationContainer>
             <StackNavigation />
         </NavigationContainer>
     );
+    */
 };
 
 export default App;
