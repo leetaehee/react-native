a7484f685a24ab07fb03d2aa9b95d416211ffbfa
leetaehee <lastride25@naver.com>
2021-04-18 23:02:01 +0900

    로그인페이지, input 컴포넌트 추가

--- react-native-simple-chat/src/screens/Login.js
+++ react-native-simple-chat/src/screens/Login.js
@@ -1,20 +1,62 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components/native';
-import { Text, Button } from 'react-native';
+import { Image, Input } from '../components';
+import { images } from '../utils/images';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { KeyboardAvoidingView } from 'react-native';
 
 const Container = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
     background-color: ${({ theme }) => theme.background};
+    padding: 20px;
 `;
 
 const Login = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const passwordRef = useRef();
+
+    /*
+        키보드 감추기.. 스크롤은 되지 않음 
+
+        import { TouchableWithoutFeedback, Keyboard } from 'react-native'; 추가 할 것 
+
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <Container>
+                ...
+            </Container>
+        </TouchableWithoutFeedback>     
+     */
+
     return (
-        <Container>
-            <Text style={{ fontSize: 30}}>Login Scrren</Text>
-            <Button title="Signup" onPress={() => navigation.navigate('Signup')} />
-        </Container>
+        <KeyboardAwareScrollView
+            contentContainerStyle={{ flex: 1 }}
+            extraScrollHeight={20}
+        >
+            <Container>
+                <Image url={images.logo} imageStyle={{ borderRadius: 8 }} />
+                <Input
+                    label="Email"
+                    value={email}
+                    onChangeText={text => setEmail(text)} 
+                    onSubmitEditing={() => passwordRef.current.focus()}
+                    placeholder="Email"
+                    returnKeyType="next"
+                />
+                <Input
+                    ref={passwordRef}
+                    label="Password"
+                    value={password}
+                    onChangeText={text => setPassword(text)}
+                    onSubmitEditing={() => {}}
+                    placeholder="Password"
+                    returnKeyType="done"
+                    isPassword
+                />
+            </Container>
+        </KeyboardAwareScrollView>
     );
 };
 
---
+++ react-native-simple-chat/src/utils/images.js
@@ -0,0 +1,5 @@
+const prefix = 'https://firebasestorage.googleapis.com/v0/b/react-native-simple-chat-774ca.appspot.com/o';
+
+export const images = {
+    logo: `${prefix}/logo.png?alt=media`,
+};
