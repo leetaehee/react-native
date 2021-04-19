f3ae1bae07350fb9cde6df20ded2086b6846513d
leetaehee <lastride25@naver.com>
2021-04-19 22:35:56 +0900

    로그인 버튼 추가

--- react-native-simple-chat/src/screens/Login.js
+++ react-native-simple-chat/src/screens/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components/native';
-import { Image, Input } from '../components';
+import { Image, Input, Button } from '../components';
 import { images } from '../utils/images';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { validateEmail, removeWhitespace } from '../utils/common';
 
 const Container = styled.View`
     flex: 1;
@@ -12,10 +13,39 @@ const Container = styled.View`
     padding: 20px;
 `;
 
+const ErrorText = styled.Text`
+    align-items: flex-start;
+    width: 100%;
+    height: 20px;
+    margin-bottom: 10px;
+    line-height: 20px;
+    color: ${({ theme }) => theme.errorText };
+`;
+
 const Login = ({ navigation }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [disabled, setDisabled] = useState(true);
     const passwordRef = useRef();
+
+    useEffect(() => {
+        setDisabled(!(email && password && !errorMessage));
+    }, [email. password,errorMessage]);
+
+    const _handleEmailChange = email => {
+        const changedEmail = removeWhitespace(email);
+        setEmail(changedEmail);
+        setErrorMessage(
+            validateEmail(changedEmail) ? '' : 'Please verify your email.'
+        );
+    };
+
+    const _handlePasswordChange = password => {
+        setPassword(removeWhitespace(password));
+    };
+
+    const _handleLoginButtonPress = () => {};
 
     /*
         키보드 감추기.. 스크롤은 되지 않음 
@@ -39,7 +69,7 @@ const Login = ({ navigation }) => {
                 <Input
                     label="Email"
                     value={email}
-                    onChangeText={text => setEmail(text)} 
+                    onChangeText={_handleEmailChange} 
                     onSubmitEditing={() => passwordRef.current.focus()}
                     placeholder="Email"
                     returnKeyType="next"
@@ -48,11 +78,22 @@ const Login = ({ navigation }) => {
                     ref={passwordRef}
                     label="Password"
                     value={password}
-                    onChangeText={text => setPassword(text)}
-                    onSubmitEditing={() => {}}
+                    onChangeText={_handlePasswordChange}
+                    onSubmitEditing={_handleLoginButtonPress}
                     placeholder="Password"
                     returnKeyType="done"
                     isPassword
+                />
+                <ErrorText>{errorMessage}</ErrorText>
+                <Button 
+                    title="Login" 
+                    onPress={_handleLoginButtonPress} 
+                    disabled={disabled}
+                />
+                <Button 
+                    title="Sign up with email"
+                    onPress={() => navigation.navigate('Signup')}
+                    isFilled={false}
                 />
             </Container>
         </KeyboardAwareScrollView>
--- react-native-simple-chat/src/theme.js
+++ react-native-simple-chat/src/theme.js
@@ -2,7 +2,7 @@ const colors = {
     white: '#ffffff',
     black: '#000000',
     grey_0: '#d5d5d5',
-    grey_1: '#a6a6a6',
+    grey_1: '#a6a6a6',   
     red: '#e84118',
     blue: '#3679fe',
 };
@@ -14,4 +14,8 @@ export const theme = {
     label: colors.grey_1,
     inputPlaceholder: colors.grey_1,
     inputBorder: colors.grey_1,
+    errorText: colors.red,
+    buttonBackground: colors.blue,
+    buttonTitle: colors.white,
+    buttonUnfilledTitle: colors.blue,
 };
